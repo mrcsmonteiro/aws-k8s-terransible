28903539df7ef1aaf15fe309683c1ba237054ea7
Marcos Monteiro <mrcsmonteiro@gmail.com>
2025-09-15 01:01:08 +1000

    First commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.13.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.3"
+    }
+  }
+}
